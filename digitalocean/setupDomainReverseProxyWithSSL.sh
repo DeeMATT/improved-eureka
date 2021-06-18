b0be5ea13234803b471d0bb12eb3fbec95f035e3
DeeMATT <mattode@outlook.com>
2021-06-18 08:03:17 +0100

    domain setup

--- digitalocean/setupDomainReverseProxyWithSSL.sh
+++ digitalocean/setupDomainReverseProxyWithSSL.sh
@@ -9,24 +9,24 @@ function generateDomainReverseProxy(){
         listen       80;
         server_name  $fqdn;
 
-        #charset koi8-r;
-        #access_log  /var/log/nginx/host.access.log  main;
+        location = / {
+                  proxy_pass "$proxyTarget"index.html;
+          }
+          location / {
+              proxy_pass $proxyTarget;
+              proxy_set_header User-Agent \$http_user_agent;
+              proxy_set_header X-Real-IP \$remote_addr;
+              proxy_set_header Accept-Encoding '""';
+              proxy_set_header Accept-Language \$http_accept_language;
+              proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+          }
 
-        location / {
-            proxy_pass $proxyTarget;
-            proxy_set_header User-Agent \$http_user_agent;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header Accept-Encoding '""';
-            proxy_set_header Accept-Language \$http_accept_language;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        }
     }
 
     server {
         listen       80;
         server_name  app."$fqdn";
-        #charset koi8-r;
-        #access_log  /var/log/nginx/host.access.log  main;
+
         location / {
             proxy_pass https://app.lolafinance.com;
             proxy_set_header User-Agent \$http_user_agent;
@@ -42,7 +42,8 @@ function generateDomainReverseProxy(){
   sudo rm -rf /etc/nginx/sites-enabled/"$fqdn"
   sudo ln -s /etc/nginx/sites-available/"$fqdn" /etc/nginx/sites-enabled/"$fqdn"
   sudo service nginx reload
-  sudo certbot --nginx -d $fqdn
+  sudo certbot --nginx -d $fqdn 
+  sudo certbot --nginx -d app."$fqdn"
 }
 echo "about to call function to start domain installation"
 generateDomainReverseProxy "$1" "$2"
